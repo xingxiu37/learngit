aeb80e08e1bcc348f4c7af593d7b29305b8b0090
Judy Huang <zhuoerh@gmail.com>
2017-02-28 15:56:03 -0430

    remove file

--- excel.py
+++
@@ -1,9 +0,0 @@
-import openpyxl, pprint
-
-wb = openpyxl.load_workbook('test.xlsx')
-sheet = wb.get_sheet_by_name('Sheet1')
-for i in range(1, sheet.max_row+1):
-    for j in range(1, sheet.max_column+1):
-        print sheet.cell(row = i, column = j).value
-print "test"
-print 'test2'
