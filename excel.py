b4c9ea3b767959a4f07f68df5140bd8c9cf6bdf5
xingxiu37 <zhuoerh@gmail.com>
2017-03-01 16:43:48 -0400

    Update excel.py

--- excel.py
+++ excel.py
@@ -5,5 +5,4 @@ sheet = wb.get_sheet_by_name('Sheet1')
 for i in range(1, sheet.max_row+1):
     for j in range(1, sheet.max_column+1):
         print sheet.cell(row = i, column = j).value
-print "test"
-print 'test2'
+
